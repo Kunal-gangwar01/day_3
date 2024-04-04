b7f9130e5a4f56b46ce55bab7267d953a7ab7add
Kunal <gangwarkunal327@gmail.com>
2024-04-04 18:18:23 +0530

    task completed

---
+++ index.js
@@ -0,0 +1,169 @@
+// var message = 'Hello World';
+// var age = 18;
+// console.log(message);
+// function env(){
+// console.log(message)
+// var message = "hello";
+// console.log(message)
+// message = "else";
+// console.log(message)
+// }
+// env();
+// function env(){
+//     {
+//     for(var i=0;i<5;i++)
+//     console.log('--',i);
+// }
+// console.log();
+// }
+
+// let message = "Hello";
+// let message1 = "Hello";
+
+// console.log(message)
+// console.log(message1)
+
+// const message = "hello";
+// message1 = 'else';
+// console.log(message)
+// console.log(message1)
+
+// let arr = ['a','b','c','d'];
+// arr[2]= 'abcd';
+// console.log(arr);
+
+// let str = '12';
+// let age = 20;
+// let ans = age +(str*1);
+// console.log(ans);
+
+// let str = '12.12';
+// let p = parseInt(str);
+// let n = Number (str);
+// console.log(p,n);
+
+// let a = '12';
+// let b = 12;
+
+// if(a==b){
+//     console.log('yes');
+// }
+// else{
+//     console.log('no');
+//     }
+
+// let a = ['a','b','c','d'];
+// let cpy = a;
+// console.log(a, cpy);
+
+
+// const str = 'Khan';
+// const usrname = 'MY name is :';
+// const ans =`${usrname} ${str}`;
+// console.log(ans);
+
+// const a= 'My';
+// const b = 'name';
+// const c = 'is';
+// const d = ':';
+// const str = 'Khan';
+// const ans = `${a} ${b} ${c}${d} ${str}`;
+// console.log(ans);
+
+// function print(str){
+//     console.log(str);
+// }
+
+// const s = 'Hello';
+// print(s);
+
+// function sum(a,b){
+//     if(a && b){
+//     console.log(a+b);
+// }
+// else if(a){
+//     console.log(a);
+// }
+// else{
+//     console.log(0);
+// }
+// }
+// sum(2);
+
+
+//function declaration 
+// function print(){
+// console.log('--');
+// }
+ 
+//function assignment ( named)
+// const a=function print(){
+//     console.log('--');
+//     }
+
+    //function assignment ( anonymous)
+// const b=function (){
+//     console.log('--');
+//     }
+
+    //arrow function aasignment
+    // const c=()=>{
+    //     console.log('--');
+    //     }
+    // print();
+    // a();
+    // b();
+
+    //switch condition
+
+//     let a = 3;
+//     switch(a){
+//         case 1:{
+// console.log('one');
+// break;
+// }   
+// case 2:{
+//     console.log('two');
+// break;
+// }
+// case 3:{
+//     console.log('three');
+//     break;
+// }
+// default:{
+//     console.log('not 1,2,3');
+// }
+//  }
+
+
+// const obj = new Object();
+// obj[1] = 'one';
+// obj[2] = 'two';
+// obj[12] = 'twelve';
+// console.log(obj);
+
+
+// const obj = {
+//    'firstName':'kunal',
+//     'lastName': 'gangwar',
+//     'age': 20,
+// };
+// console.log(obj);
+
+
+// const fullName1 = obj.firstname + ' ' + obj.lastName;
+
+// const fullName2 = `${obj.firstName} ${obj.lastName}`;
+// console.log(fullName);
+// console.log(fullname);
+
+
+//prompt
+
+// const obj = {
+//     'firstName':'kunal',
+//      'lastName': 'gangwar',
+//      'age': 20,
+//  };
+//  const x = prompt('Please tell the property to know');
+//  console.log(obj[x]);
